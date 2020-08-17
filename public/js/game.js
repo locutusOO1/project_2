c7599295a2756f0c33ffc54e75cee2bb6660c227
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-08-17 16:29:32 -0500

    fixed boolean answers issue

--- public/js/game.js
+++ public/js/game.js
@@ -29,16 +29,12 @@
                    options.sort();
                    let newQuestions = $(`
                    <h3 class="roll">Question: ${results[i].question}</h3>
-                   <p>Choose answer: 
-                   <ul>
-                   <button class="btn btn-primary btn-rounded roll">${results[i].incorrect_answers[0]} </button>
-                   <button class="btn btn-primary btn-rounded roll">${results[i].incorrect_answers[1]} </button>
-                   <button class="btn btn-primary btn-rounded roll">${results[i].incorrect_answers[2]} </button>
-                   <button class="btn btn-primary btn-rounded roll">${results[i].incorrect_answers[3]} </button>
-                   </ul>
-                   </p>
-                   `);
-                quesDiv.append(newQuestions)
+                   <p>Choose answer:</p>`);
+                   quesDiv.append(newQuestions)
+                for (let j = 0; j < options.length; j++) {
+                    let newAnswers = $(`<p><button class="btn btn-primary btn-rounded roll">${options[j]} </button></p>`)
+                    quesDiv.append(newAnswers)
+                }
                 }
                
                 console.log(results)
